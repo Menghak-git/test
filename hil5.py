6b9d44209eb4f1e84380077c5f2b6776b17b9d7c
Menghak Sao <ghh434419@gmail.com>
2026-09-15 10:48:38 +0700

    menghak

--- hil5.py
+++ hil5.py
@@ -1,8 +1,11 @@
 for i in range (20):
   if i %2 == 0:
-input(len(i.)) 
-     print(i)
+    input(len(1)) 
+  print(i)
 
 
+# for i in range (12):
+#   if i %2==1:
+# input(len(1))
+#         print(i)
 
-    
